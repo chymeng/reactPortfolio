0c2a6e0c7656ae07c2262334b3d51d9aad4acaad
Chy <chymeng24@gmail.com>
2017-12-28 20:22:27 +1100

    Footer hover effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+var FontAwesome = require('react-fontawesome')
 
 class Footer extends Component {
 	render() {
@@ -7,10 +8,51 @@ class Footer extends Component {
 				<div className="footerContent">
 					<div className="contactList">
 						<ul>
-							<li>GitHub</li>
-							<li>LinkedIn</li>
-							<li>Say hi</li>
-							<li>Blog</li>
+							<li>
+								<a href="https://github.com/chymeng" target="_blank">
+									<FontAwesome
+										className="githubIcon"
+										name="github"
+										size="10x"
+										tag="i"
+									/>
+									GitHub
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.linkedin.com/in/chy-meng-6323b8a7/"
+									target="_blank"
+								>
+									<FontAwesome
+										className="linkedInIcon"
+										name="linkedin"
+										size="10x"
+										tag="i"
+									/>
+									LinkedIn
+								</a>
+							</li>
+							<li>
+								<a href="mailto:chymeng24@gmail.com" target="_blank">
+									<img
+										src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/waving-hand-sign_1f44b.png"
+										target="_blank"
+										className="handWaveIcon"
+									/>
+									Say hi
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<FontAwesome
+										className="blogIcon"
+										name="book"
+										size="10x"
+										tag="i"
+									/>Blog
+								</a>
+							</li>
 						</ul>
 					</div>
 				</div>
